751732750c56f732aec2bd6e486896ba82f138db
Tran Dai Loi <b1505902@gmail.com>
2018-04-02 09:53:00 +0700

    Add Grade

--- resource/grade/AddGrade.php
+++ resource/grade/AddGrade.php
@@ -2,6 +2,18 @@
     <div class="row">
     <div class="form-group form-control">
         <div class="text-bold text-center col-2">
+        <form method="post" class="">
+					<div class="form-group">
+						<h4 class="text-center">Add Grade</h4>
+					</div>
+					<div class="form-group">
+						<label for="txtSach">Grade Names</label>
+						<input type="text" class="form-control" id="txtSach" name="txtSach" placeholder="Tên Sách">
+					</div>
+					
+					<input type="submit" class="btn btn-danger" name="btnAdd" value="Thêm Mới"/>
+					<input type="reset" name="btnReset" value="Nhập Lại" class="btn btn-info" />
+				</form>
         </div>
     </div>
 </div>
